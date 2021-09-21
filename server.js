b0085fe8e17dbaea73ef516e64d91270d9c59954
Nghia Pham Duc <mpdn99@gmail.com>
2021-09-21 19:55:48 +0700

    init

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (request, response) => {
     port:'3306',
     user: "u960615773_mobile",
     password: "Istech123",
-    database: "u960615773_b2_app"
+    database: "u960615773_bunny2"
 });
 
 // app.get('/category', function (req, res) {
@@ -38,47 +38,47 @@ app.get('/category/:id', function (req, res) {
     }
     dbConn.query('SELECT * FROM tests where category_id=?', category_id, function (error, results, fields) {
      if (error) throw error;
-      return res.send({ error: false, data: results, message: 'category.' });
+      return res.send({ error: false, data: results, message: 'category list.' });
     });
 });
 
-app.get('/test', function (req, res) {
-    dbConn.query('SELECT * FROM tests', function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'tests list.' });
-    });
-});
+// app.get('/test', function (req, res) {
+//     dbConn.query('SELECT * FROM tests', function (error, results, fields) {
+//         if (error) throw error;
+//         return res.send({ error: false, data: results, message: 'tests list.' });
+//     });
+// });
 
 app.get('/test/:id', function (req, res) {
     let test_id = req.params.id;
     if (!test_id) {
      return res.status(400).send({ error: true, message: 'Please provide test_id' });
     }
-    dbConn.query('SELECT * FROM parts where part_id=?', test_id, function (error, results, fields) {
+    dbConn.query('SELECT * FROM parts where test_id=?', test_id, function (error, results, fields) {
      if (error) throw error;
-      return res.send({ error: false, data: results, message: 'test.' });
+      return res.send({ error: false, data: results, message: 'test list.' });
     });
 });
 
-app.get('/questions', function (req, res) {
-    dbConn.query('SELECT * FROM questions', function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'questions list.' });
-    });
-});
+// app.get('/part/', function (req, res) {
+//     dbConn.query('SELECT * FROM questions', function (error, results, fields) {
+//         if (error) throw error;
+//         return res.send({ error: false, data: results, message: 'questions list.' });
+//     });
+// });
 
-app.get('/test/questions/:id', function (req, res) {
-    let question_id = req.params.id;
-    if (!question_id) {
-     return res.status(400).send({ error: true, message: 'Please provide question_id' });
+app.get('/test/part/:id', function (req, res) {
+    let part_id = req.params.id;
+    if (!part_id) {
+     return res.status(400).send({ error: true, message: 'Please provide part_id' });
     }
-    dbConn.query('SELECT * FROM question where question_id=?', question_id, function (error, results, fields) {
+    dbConn.query('SELECT * FROM question where part_id=?', part_id, function (error, results, fields) {
      if (error) throw error;
-      return res.send({ error: false, data: results, message: 'question.' });
+      return res.send({ error: false, data: results, message: 'question list.' });
     });
 });
 
-app.get('/test/question/:id/answer', function (req, res) {
+app.get('/test/part/question/:id', function (req, res) {
     let question_id = req.params.id;
     if (!question_id) {
      return res.status(400).send({ error: true, message: 'Please provide question_id' });
